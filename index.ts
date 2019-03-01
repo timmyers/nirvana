0a14945a95f04ca254d9a93d51bc0c4b02b632a3
Tim Myers <timmyers@gmail.com>
2019-02-28 19:29:11 -0700

    Add eslint

--- index.ts
+++ index.ts
@@ -1,56 +1,55 @@
-import * as aws from '@pulumi/aws';
-import * as pulumi from '@pulumi/pulumi';
-import { AWSK8SCluster } from './src/eks_cluster';
 import { GCPK8SCluster } from './src/gcp_cluster';
 import { K8SOpsView } from './src/k8s_opsview';
-import { K8SJenkins } from './src/k8s_jenkins';
-import { K8SIngress } from './src/k8s_ingress';
-import { K8SArgo } from './src/k8s_argo';
-import { K8SKubeStateMetrics } from './src/k8s_kube_state_metrics';
+// import { K8SIngress } from './src/k8s_ingress';
+// import { K8SArgo } from './src/k8s_argo';
 import { GCPDNSZone } from './src/gcp_dns_zone'
 import { NameComNameservers } from './src/name_com_nameservers'
 import { GCPIdentity } from './src/identity/gcp';
 
-
 (async () => {
-  const dnsZone = new GCPDNSZone("tmye.me", {
-    dnsName: "tmye.me."
+  const config = {
+    opsView: false,
+  };
+
+  const dnsZone = new GCPDNSZone('tmye.me', {
+    dnsName: 'tmye.me.',
   });
 
-  const nameServers = new NameComNameservers("tmye.me", {
-    domainName: "tmye.me",
-    nameServers: dnsZone.dnsZone.apply(d => d.nameServers)
+  const nameServers = new NameComNameservers('tmye.me', {
+    domainName: 'tmye.me',
+    nameServers: dnsZone.dnsZone.apply(d => d.nameServers),
   });
 
-  const gcpIdentity = new GCPIdentity("infra", {
-    project: 'nirvana-232117'
-  })
+  const gcpIdentity = new GCPIdentity('infra', {
+    project: 'nirvana-232117',
+  });
 
   // gcpIdentity.infraCISecret.apply(s => console.log(JSON.stringify(s)));
 
-  const cluster = new GCPK8SCluster("cluster", {
+  const cluster = new GCPK8SCluster('cluster', {
     machineType: 'g1-small',
     externalDns: true,
     certManager: true,
     prometheus: false,
   });
 
-  // const defaultOpts = {
-  //   providers: {
-  //     kubernetes: cluster.k8sProvider
-  //   }
-  // };
+  const defaultOpts = {
+    providers: {
+      kubernetes: cluster.k8sProvider,
+    },
+  };
 
-  // const opsView = new K8SOpsView("opsview", {}, defaultOpts);
-  // const jenkins = new K8SJenkins("jenkins", {}, defaultOpts);
+  if (config.opsView === undefined || config.opsView) {
+    const opsView = new K8SOpsView('opsview', {}, defaultOpts);
+  }
 
-  // const ingress = new K8SIngress("ingress", {}, {
+  // const jenkins = new K8SJenkins('jenkins', {}, defaultOpts);
+
+  // const ingress = new K8SIngress('ingress', {}, {
   //   ...defaultOpts,
   //   dependsOn: [opsView, jenkins]
   //   // dependsOn: [opsView]
   // })
 
-  // const argo = new K8SArgo("argo", {}, defaultOpts);
-})()
-
-
+  // const argo = new K8SArgo('argo', {}, defaultOpts);
+})();
